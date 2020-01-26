dcced190ed326e871dd05dc4947927294e2426f3
luisito666 <luispenagos91@gmail.com>
2020-01-26 10:33:46 -0500

    Finix details

--- src/frontend/src/components/users/index.tsx
+++ src/frontend/src/components/users/index.tsx
@@ -6,23 +6,45 @@ import { connect } from 'react-redux';
 
 import * as usersActions from '../../store/actions/usersActions';
 
-interface IProps {}
+interface IProps {
+    loginReducers: {username: ''};
+    usersReducers: {users: []}
+}
 
-interface Istate {}
+interface Istate {
+    users: []
+}
 
 
-class UserComponent extends React.Component<IProps, Istate> {
+class UserComponent extends React.Component<IProps, any> {
     render() {
+        console.log(this.props);
         return (
             <div className="chat-users mt-2">
 
+                <h1> Online </h1>                
+                <hr/>
+                <div className="chat-users-scroll" id="chat-users-scroll">
+
+                {this.props.usersReducers.users.map((element: any, index: number) => (
+                    <div>
+                        <span className={"d-inline badge " + (this.props.loginReducers.username === element.name ? 'badge-success' : 'badge-secondary')} key={index}>
+                            {element.name}
+                        </span>
+                    </div>
+                ))}  
+
+                </div>          
             </div>
         )
     }
 }
 
-const mapStateToProps = ({usersReducers}: {usersReducers: any}) => {
-    return usersReducers
+const mapStateToProps = ({usersReducers, loginReducers}: {usersReducers: any,  loginReducers: any}) => {
+    return {
+        usersReducers,
+        loginReducers
+    }
 }
 
 export default connect(mapStateToProps, usersActions)(UserComponent);
